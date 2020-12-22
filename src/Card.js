36cdac1086d2abce1b1b2af4846f5efe95da9b24
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-22 16:05:23 +0530

    Completed Cards

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,11 @@ function Card({ src, title, description, price }) {
     return (
         <div className="card">
             <img src={src} alt="" />
+            <div className="card__info">
+                <h2>{title}</h2>
+                <h4>{description}</h4>
+                <h3>{price}</h3>
+            </div>
         </div>
     )
 }
