0c757b38d3b7bb72d9ce790179e980898c18a4b7
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-22 16:22:51 +0530

    Setting up react-router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+    <Router>
+        <Header />
+
+        <Home />
+
+        <Footer />
+    </Router>
     </div>
   );
 }
