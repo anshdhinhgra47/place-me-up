38d3af5fd18ec9c0d19eae1391a50d6f640e2f55
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-22 16:19:20 +0530

    Completed page_footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="page__footer">
+            <p>&copy; Airbnb clone! No rights 
+            reserved - this is only a demo!</p>
+            <p>Privacy Terms Sitemap Company Details</p>
+        </div>
+    )
+}
+
+export default Footer;
